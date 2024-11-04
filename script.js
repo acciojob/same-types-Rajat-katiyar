89cd86e4ecec2f03220473013fee7864d45f8718
Rajat Katiyar <123207263+Rajat-katiyar@users.noreply.github.com>
2024-11-05 05:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function isSameType(value1, value2) {
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
+console.log(isSameType(1, 3));        
+console.log(isSameType("hey", "hello")); 
+console.log(isSameType(NaN, NaN));   
+console.log(isSameType("3", 3));     
+
